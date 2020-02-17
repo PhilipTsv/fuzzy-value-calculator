b8c9a2691f70cd91850ceb99a8cec2d08d6fd842
PhilipTsv <filip.tsvetanov@abv.bg>
2020-02-17 10:48:57 +0000

    Fuzzification procedure completed. Changed example to match textbook for easier verification of answers.

--- venv/RuleParser.py
+++ venv/RuleParser.py
@@ -14,7 +14,7 @@ def hasNumbers(inputString):
 
 lines = file.read().splitlines()
 
-#removnig the first line
+#removnig the first line - the title
 lines = lines[1:len(lines)]
 
 #removing empty lines
@@ -34,9 +34,9 @@ lines = [x for x in lines if x not in values]
 #remove rules from lines
 lines = [x for x in lines if x not in rules]
 
-print(lines)
-print(values)
-print(rules)
+#print(lines)
+#print(values)
+#print(rules)
 
 upperName = ""
 for  x in range(0, len(lines)):
@@ -76,8 +76,63 @@ def getSet(upperName, name):
 #print(getSet('driving','bad'))
 
 def calcAsc(a, alpha, value):
-    return (alpha -a  + value) / alpha
+    return (alpha - a  + value) / alpha
 
 def calcDesc(b, beta, value):
     return (b + beta - value) / beta
+
+print(values)
+
+def valueToVars(str):
+    name = ""
+    value = 0
+
+    #getting the words from the statement and stripping white space
+    words = str.split(" ")
+    words = list(filter(None, words))
+
+    for word in words:
+        word = word.strip()
+        if hasNumbers(word) is False and word is not "=":
+            name = word
+        elif (hasNumbers(word)):
+            value = word
+
+    return name,value
+
+def calculate_fuzzy(inputTuple):
+    name = inputTuple[0]
+    value = int(inputTuple[1])
+    fuzzyDict = fuzzyDictionary[name]
+    output = []
+
+    # for each area in fuzzy values
+    for dic in fuzzyDict:
+        ranges = list(dic.values())[0]
+        a = ranges[0]
+        b = ranges[1]
+        alpha = ranges[2]
+        beta = ranges[3]
+
+        #if fuzzines of value is 1
+        if (value > a and value < b) or (value == a or value == b):
+            output.append(1)
+        #if fuziness is within alpha - a
+        elif (value > (a - alpha) and value < a):
+            output.append(calcAsc(a, alpha, value))
+        #if fuziness is within b - beta
+        elif (value > b and value < (beta + b)):
+            output.append(calcDesc(b, beta, value))
+        #all other cases - a few of them - fuziness is 0
+        else:
+            output.append(0)
+
+    return output
+
+fuzzyValues = []
+for value in values:
+    fuzzyValues.append(calculate_fuzzy(valueToVars(value)))
+
+print(fuzzyValues)
+
 
